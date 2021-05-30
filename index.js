2c7a23b9efd0cde4badc356e6d7253b5a2280bae
loras26 <nyambegugu@gmail.com>
2021-05-30 11:49:19 +0200

    modified file

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.render('index', {
-        settings: settingsBill.getSettings(),
+       /* settings: settingsBill.getSettings(),*/
         totals: settingsBill.totals()
     });
 })
